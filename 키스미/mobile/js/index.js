9773279cd34f894f1ab1575f295a587dde415637
yh9887 <gmlakddl987@naver.com>
2023-01-26 23:13:37 +0900

    commit!

--- 키스미/mobile/js/index.js
+++ 키스미/mobile/js/index.js
@@ -56,75 +56,6 @@ var swiper = new Swiper(".mySwiper1", {
   },
 });
 
-/* *********************************** FullPage  ********************************** */
-$(function () {
-
-  $('#fullpage').fullpage({
-    //options here
-    autoScrolling: true,
-    scrollHorizontally: true,
-    anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
-    navigation: true,
-    navigationPosition: 'right',
-    // responsiveWidth: 1024,    
-    afterLoad: function (anchorsLink, index) {
-      if (index == 1) {
-        $('.top_btn').css({ opacity: '0' })
-      } else {
-        $('.top_btn').css({ opacity: '1' })
-      }
-
-      if ( index == 2) {
-        ham($('.main_txt1 h1, .main_txt1 h1 span'), 'translateY(0px) ', '1', '0')
-        ham($('.main_txt1 h1 div'), 'translateX(140%) ', '1', '0')
-        ham($('.brand_logo '), 'translateY(0px)', '1', '500');
-        ham($('.brand_border'), 'scale(1)', '1','700');
-        ham($('.brand_txt1'), 'translateY(0px)', '1','1000');
-        ham($('.brand_txt2'), 'translateY(0px)', '1','1200');
-      }
-      else{
-        $('.main_txt1 h1, .main_txt1 h1 span, .main_txt1 h1 div, .brand_logo, .brand_border, .brand_txt1, .brand_txt2').removeAttr("style");
-      }
-      
-      if(index == 3){
-        ham($('.main_txt2'), 'translateY(0px)', '1','20');
-        ham($('.info_notice'), 'translate(-50%, -50%)', '1','20');
-        ham($('.info_btn'),   'translate(-50%, -50%)', '1','50');
-      }
-      else{
-        $('.main_txt2, .info_notice, .info_btn').removeAttr("style");
-      }
-      
-      if(index == 4){
-        ham($('.shop_menu h1'),'', '1','0');
-        ham($('.white_box'),'translateX(100%)', '1','0');
-        ham($('.shop_border'), 'scale(1)', '1','80');
-        ham($('.lbl1'), 'translateY(0px)', '1','200');
-        ham($('.lbl2'), 'translateY(0px)', '1','220');
-        ham($('.lbl3'), 'translateY(0px)', '1','240');
-        ham($('.lbl4'), 'translateY(0px)', '1','260');
-        ham($('.shop_txt span'), 'translateX(0px)', '1','500');
-        ham($('.shop_txt input'), 'translate(-50%, -50%)', '1','600');
-      }
-      else{
-        $('.shop_menu h1, .white_box, .shop_border, .lbl, .shop_txt span, .shop_txt input').removeAttr("style");
-      }
-
-      if(index == 5){
-        ham($('.instar_menu h1'),'translateY(0px)', '1','20');
-        ham($('.instar_menu div'),'', '1','200');
-        ham($('.feed_wrap'),'translateY(0px)', '1','20');
-        ham($('.instar_feed'),'', '1','300');
-      }
-      else{
-        $('.instar_menu h1, .instar_menu div, .feed_wrap, .instar_feed').removeAttr("style");
-      }
-    }
-
-    
-    
-  });
-})
 
 function ham(el, trans, opa, time ) {
   setTimeout(() => {
@@ -135,10 +66,24 @@ function ham(el, trans, opa, time ) {
   }, time);
 }
 
-/* *********************************** section2  ********************************** */
+/* *********************************** 헤더 스크롤  ********************************** */
 $(document).ready(function () {
+
+
+
   $(window).scroll(function () {
+      let s_top = $(window).scrollTop();
 
+      // 헤더
+      if (s_top > 300) {
+          $('.header').css({
+            background: '#b4091fb3',
+            borderBottom: '0.1px solid #ffffff0f'
+          })
+      }
+      else{
+        $('.header').removeAttr("style");
+      }
+  });
 
-  })
-})
+});
