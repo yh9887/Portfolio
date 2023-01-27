6026e7ecf71450361a736ddfc8067e75f9649e95
yh9887 <gmlakddl987@naver.com>
2023-01-27 19:48:48 +0900

    ㅗ

--- 키스미/mobile/js/product.js
+++ 키스미/mobile/js/product.js
@@ -10,31 +10,58 @@ $(document).ready(function () {
 
 
     function ham(el, trans, opa, time) {
-                setTimeout(() => {
-                    el.css({
-                        transform: trans,
-                        opacity: opa,
-                    })
-            }, time);
-        }
+        setTimeout(() => {
+            el.css({
+                transform: trans,
+                opacity: opa,
+            })
+        }, time);
+    }
 
-        ham($('.title_name'),'translateY(0)', '1','0');
-        ham($('.title_banner'),'translateY(0)', '1','0');
+    ham($('.title_name'), 'translateY(0)', '1', '0');
+    ham($('.title_banner'), 'translateY(0)', '1', '0');
 
     $(window).scroll(function () {
         let s_top = $(window).scrollTop();
-        
+
         // ham($('.title_name'),'translateY(0)', '1','0');
         // ham($('.title_banner'),'translateY(0)', '1','0');
 
-        if(s_top >= 600 && s_top <5000){
-            ham($('.product_content'),'translateY(0)', '1','0');
+        if (s_top >= 600 && s_top < 5000) {
+            ham($('.product_content'), 'translateY(0)', '1', '0');
         }
-        else{
+        else {
             $('.product_content').removeAttr("style");
-        } 
-        
+        }
+
+
+
+        // 헤더
+        if (s_top > 100) {
+            $('.header').css({
+                background: '#ffe9e9c2',
+            })
+        }
+        else {
+            $('.header').removeAttr("style");
+        }
+        //
     });
+    let ham_chk = true;
+    $(document).on('click', '.hambug', function(){
+  
+      if(ham_chk) {
+        // 햄버거 => X 관련 코드  
+        $(".hambug").toggleClass('hamimg_active')
+        $('.aside').toggleClass('ham_active') ;
+    }
+    
+    else { 
+        // X => 햄버거 관련 코드   
+        $(".hambug").toggleClass('hamimg_active')  
+        $('.aside').toggleClass('ham_active') ;
+      }  
+  });
 
 })
 
