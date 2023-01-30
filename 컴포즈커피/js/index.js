8f7f3d7f6d368c9d4762e19b38276fa211e9cf6b
yh9887 <gmlakddl987@naver.com>
2023-01-30 15:31:46 +0900

    gg

--- 컴포즈커피/js/index.js
+++ 컴포즈커피/js/index.js
@@ -1,13 +1,27 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        let s_top = $(window).scrollTop();
-        // 헤더
-        if (s_top > 100) {
-            $('.header').css({
-                background: 'white',
-                borderBottom: 'none'
-            })
-        }
-        console.log(s_top)
-    })
+
+window.addEventListener('scroll', function () {
+
+    let s_top = $(window).scrollTop();
+    if (s_top > 100) {
+        $('.header').css({
+            background: 'white',
+            borderBottom: 'none',
+            top: 0
+        })
+        $('.menu_li').css({
+            color: 'black'
+        })
+    }
+    else {
+        $('.header, .menu_li').removeAttr("style");
+    }
+
+    let header = document.querySelector(".header");
+    let headerHeight = header.offsetHeight;
+    let windowTop = window.scrollY;
+    if (windowTop >= headerHeight) {
+        header.classList.add("drop");
+    } else {
+        header.classList.remove("drop");
+    }
 });
